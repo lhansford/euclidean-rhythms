6f4c0e9660f613e9e77b8b8e8c2fb3758dc7b581
Luke Hansford <mail@lukehansford.me>
2018-04-01 08:35:23 +0800

    Get Single File Components working

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">
+		<h1>My App!</h1>
+    <p>{{ test }}</p>
+	</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+    data() {
+      return { test: 'test' };
+    }
+  }
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('!')
+import Vue from 'vue';
+
+import App from './App.vue';
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App },
+});
