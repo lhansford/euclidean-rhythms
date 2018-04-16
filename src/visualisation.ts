d8673cbb4e8513ba6af9bcea3847fe0ba731286c
Luke Hansford <mail@lukehansford.me>
2018-04-16 20:33:05 +0800

    Add documentation to visualistion.ts

--- src/visualisation.ts
+++ src/visualisation.ts
@@ -1,18 +1,25 @@
 
-export function getPolygonPoints(sides: number, radius: number) {
+/**
+ * Returns an array of coordinates (represented as an array with an x and y value) that can be used
+ * to draw a polygon with sides and radius matching the given values.
+ */
+export function getPolygonPoints(sides: number, radius: number): Array<Array<number>> {
   let i = 0;
   const points = [];
   while (i < sides) {
-    const point = [
+    points.push([
       radius * Math.cos(2 * Math.PI * i / sides) + radius,
       radius * Math.sin(2 * Math.PI * i / sides) + radius,
-    ];
-    points.push(point);
+    ]);
     i += 1;
   }
   return points
 }
 
+/**
+ * Takes an array of coordinate tuples (e.g. [x, y]) and converts them to a string acceptable
+ * by a HTML svg element.
+ */
 export function polygonPointsToString(points: Array<Array<number>>): string {
   return points.map(x => x.join(',')).join(' ');
 }
